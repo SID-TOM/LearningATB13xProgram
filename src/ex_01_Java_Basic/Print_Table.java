b4119e83f0b7f54fd838e0f140d81922159b98a3
SID-TOM <sidtomar87@gmail.com>
2025-06-11 06:52:27 +0530

    Hello

---
+++ src/ex_01_Java_Basic/Print_Table.java
@@ -0,0 +1,19 @@
+package ex_01_Java_Basic;
+
+public class Print_Table {
+    public static void main(String[] args) {
+        int a=9,b=1;
+        System.out.printf("%d x %d = % d\n",a,b,a*b);
+        System.out.printf("%d x %d = % d\n",a,b,a*b*2);
+        System.out.printf("%d x %d = % d\n",a,b,a*b*3);
+        System.out.printf("%d x %d = % d\n",a,b,a*b*4);
+        System.out.printf("%d x %d = % d\n",a,b,a*b*5);
+        System.out.printf("%d x %d = % d\n",a,b,a*b*6);
+        System.out.printf("%d x %d = % d\n",a,b,a*b*7);
+        System.out.printf("%d x %d = % d\n",a,b,a*b*8);
+        System.out.printf("%d x %d = % d\n",a,b,a*b*9);
+        System.out.printf("%d x %d = % d\n",a,b,a*b*10);
+
+    }
+
+}
