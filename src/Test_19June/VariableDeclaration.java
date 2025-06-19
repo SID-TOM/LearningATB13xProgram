a9916e391d15b679790aa09b0172098af210a998
SID-TOM <sidtomar87@gmail.com>
2025-06-19 07:54:20 +0530

    Hello

---
+++ src/Test_19June/VariableDeclaration.java
@@ -0,0 +1,14 @@
+package Test_19June;
+
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        int VDInteger = 20;
+        double VDDouble = 3.14;
+        boolean VDbool = true;
+        char VDcharacter = 'A';
+        System.out.println("Integer:" + VDInteger);
+        System.out.println("Double:" + VDDouble);
+        System.out.println("Boolean:" + VDbool);
+        System.out.println("Char:" + VDcharacter);
+    }
+}
